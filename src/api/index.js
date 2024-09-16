73895b1d94500c084cf23d15763f96d4fe5e4ddf
PongoDev <90353497+endlessalpacaYT@users.noreply.github.com>
2024-09-16 02:29:38 +0100

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -5,6 +5,14 @@ const app = express();
 
 const PORT = process.env.API_PORT || 5555
 
+app.get('/', (req, res) => {
+    res.json({
+        api: "Arcane_API",
+        version: "v0.1",
+        started: Date.now()
+    })
+});
+
 async function startHTTPServer() {
     app.listen(PORT, () => {
         console.log(`Arcane API Listening on 127.0.0.1:${PORT}`);
